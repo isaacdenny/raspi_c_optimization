23cc31d80304dadb59c30cbef76cac6d58631602
Graham McCabe <grahamamccabe@gmail.com>
2024-11-11 14:08:02 -0500

    constant_folding update

--- constant_folding/optimized.c
+++ constant_folding/optimized.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 #define N 5
+#define SIZE 1000000
 
 int main() {
-    int debug = 0;  // Constant folding applied: N - 5 evaluated to 0
+    clock_t start, end;
+
+    start = clock();
+
+    for (int i = 0; i < SIZE; i++) {
+        int debug = 0;  // Constant folding applied: N - 5 evaluated to 0
+    }
+    end = clock();
+
     return 0;
 }
--- constant_folding/unoptimized.c
+++ constant_folding/unoptimized.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <time.h>
 
 #define N 5
+#define SIZE 1000000
 
 int main() {
-    int debug = N - 5;  // Calculation written as-is, without direct optimization
+    clock_t start, end;
+
+    start = clock();
+
+    for (int i = 0; i < SIZE; i++) {
+        int debug = N - 5;  // Calculation written as-is, without direct optimization
+    }
+    end = clock();
     return 0;
 }
