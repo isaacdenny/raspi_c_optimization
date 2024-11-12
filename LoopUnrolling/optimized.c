9e90c2c87a2883dedc7bebb3e3cecbaa3cc8e461
Vatvo <34244664+Vatvo@users.noreply.github.com>
2024-11-11 22:21:26 -0500

    loop unrolling

---
+++ LoopUnrolling/optimized.c
@@ -0,0 +1,14 @@
+// helper function: checks to see if a number is prime
+int isPrime(int x) {
+    int i;
+    int max = sqrt(x)+1;
+
+    // no prime number is less than 2
+    for (i = 2; i < max; i+=2) {
+        // if the number is divisible by i or i+1
+        if ( (x % i == 0) || (x % (i+1) == 0) ) {
+            return 0; // it's not prime
+        }
+    }
+    return 1; // otherwise it is
+}
---
+++ LoopUnrolling/unoptimized.c
@@ -0,0 +1,14 @@
+// helper function: checks to see if a number is prime
+int isPrime(int x) {
+    int i;
+    int max = sqrt(x) + 1;
+
+    // no prime number is less than 2
+    for (i = 2; i < max; i++) {
+        // if the number is divisible by i
+        if (x % i == 0) {
+            return 0; // it's not prime
+        }
+    }
+    return 1; // otherwise it is
+}
